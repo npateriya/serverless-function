55ae98770799b1f3563d268926010d5ce4122334
nehayward <nehayward@gmail.com>
2016-10-28 17:19:37 -0500

    Added update function

--- ui/js/index.js
+++ ui/js/index.js
@@ -7,6 +7,7 @@ function init() {
     loadItems();
     addBtn();
     run();
+    update();
     modal();
 }
 
@@ -16,8 +17,8 @@ var currentIndex;
 function run() {
     $(document).ready(function() {
         $('#runButton').click(function() {
-          // runFunction(array[0].name)
-            if (document.getElementById("results") != null){
+            // runFunction(array[0].name)
+            if (document.getElementById("results") != null) {
                 document.getElementById("results").remove();
             }
             if (typeof(currentIndex) != "undefined") {
@@ -31,29 +32,23 @@ function run() {
     });
 }
 
+function update() {
+    $(document).ready(function() {
+        $('#updateButton').click(function() {
+            if (typeof(currentIndex) != "undefined") {
+                updateFunction(array[currentIndex].name)
+            } else {
+                var modal = document.getElementById('myModal');
+                modal.style.display = "inline-block"
+            }
+        });
+    });
+}
+
 function showPage() {
     document.getElementById("loader").style.display = "none";
     // document.getElementById("myDiv").style.display = "block";
 }
-
-
-
-// var values = [{
-//     name: 'Jonny Strömberg',
-//     born: 1986
-// }, {
-//     name: 'Jonas Arnklint',
-//     born: 1985
-// }, {
-//     name: 'Martina Elm',
-//     born: 1986
-// }];
-
-
-// userList.add({
-//     name: "Gustaf Lindqvist",
-//     born: 1983
-// });
 
 
 function loadItems() {
@@ -93,7 +88,7 @@ function loadFile(index) {
     if (array[index].type == "FUNCTION_TYPE_BLOB") {
         console.log(array[index].name);
         decodedString = atob(array[index].sourceblob);
-          editor.setValue(decodedString);
+        editor.setValue(decodedString);
     } else {
         $.ajax({
             url: array[index].sourceurl,
@@ -133,8 +128,55 @@ function runFunction(fileName) {
             console.log(data)
             var json = JSON.stringify(data, null, 4)
                 // $().appendTo('#results');
-                console.log(json);
-                output(syntaxHighlight(json));
+            console.log(json);
+            output(syntaxHighlight(json));
+
+
+        },
+        error: function(request, error) {
+            alert("Error: " + JSON.stringify(request));
+            cl.hide(); // Hidden by default
+
+        }
+    });
+}
+
+function updateFunction(fileName) {
+    var cl = new CanvasLoader('canvasloader-container2');
+    cl.setColor('#1475b5'); // default is '#000000'
+    cl.setShape('spiral'); // default is 'oval'
+    cl.setDiameter(15);
+    cl.show(); // Hidden by default
+
+    console.log("UPDATE");
+    // Update
+    //curl -H "Content-Type: application/json" -s -X PUT localhost:8888/function -d '{"name":"tropo2", "version":"2.1"}'
+    var milliseconds = (new Date).getTime();
+    var string = milliseconds.toString();
+
+    console.log(fileName, milliseconds);
+    var stringToEncode = editor.getValue();
+    console.log(stringToEncode);
+    encodedString = btoa(stringToEncode);
+
+    $.ajax({
+        url: "http://128.107.18.112:8888/function",
+        type: 'PUT',
+        dataType: 'json',
+        data: JSON.stringify({
+            name: fileName,
+            version: string,
+            sourceblob: encodedString
+        }),
+        success: function(data) {
+            cl.hide(); // Hidden by default
+
+            console.log(data)
+            var json = JSON.stringify(data, null, 4)
+                // $().appendTo('#results');
+            console.log(json);
+            alert('Success');
+            loadItems()
 
 
         },
@@ -147,12 +189,18 @@ function runFunction(fileName) {
 }
 
 
+
 function addBtn() {
     $('ul li h3').live('click', function() {
         var index = $(this).parent('li').index();
         console.log(array[index].name);
         loadFile(index);
         currentIndex = index
+
+       $('ul li h3').css('color','black');
+        $(this).css({
+             "color":"gray"
+         });
     });
 }
 
@@ -180,16 +228,16 @@ function modal() {
 }
 
 function output(inp) {
-  var param2 = document.getElementById("run")
-  var child = document.createElement('pre')
-  child.setAttribute("id", "results");
-  param2.appendChild(child).innerHTML = inp;
+    var param2 = document.getElementById("run")
+    var child = document.createElement('pre')
+    child.setAttribute("id", "results");
+    param2.appendChild(child).innerHTML = inp;
 
 }
 
 function syntaxHighlight(json) {
     json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function(match) {
         var cls = 'number';
         if (/^"/.test(match)) {
             if (/:$/.test(match)) {
